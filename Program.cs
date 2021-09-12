94991e6b4cf7053ebae904971110dfbab6633f81
Chris Bezençon <41871079+zebak007@users.noreply.github.com>
2021-09-12 23:20:29 +0200

    Works finally...

--- Program.cs
+++ Program.cs
@@ -1,31 +1,76 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
+
 
 namespace dotnetcoretest
 {
     class Program
     {
+        private static DownloaderOptions options; 
+
+        private static List<Enrollment> enrollmentList = new List<Enrollment>();
+    
         static async Task Main(string[] args)
         {
             using IHost host = CreateHostBuilder(args).Build();
 
             // Application code should start here.
-            string[] words = { "bot", "apple", "apricot" };
-            int minimalLength = words
-                    .Where(w => w.StartsWith("a"))
-                    .Min(w => w.Length);
-            Console.WriteLine(minimalLength);   // output: 5
 
-            int[] numbers = { 4, 7, 10 };
-            int product = numbers.Aggregate(1, (interim, next) => interim * next);
-            Console.WriteLine(product);   // output: 280
+            Console.WriteLine($"options.BearerToken={options.BearerToken}");
+            Console.WriteLine($"options.NumberOfMonthsToDownload={options.NumberOfMonthsToDownload}");
+
+            foreach (Enrollment enrollment in enrollmentList)
+            {
+                Console.WriteLine($"Enrollment number = {enrollment.EnrollmentNumber}");
+                Console.WriteLine($"Enrollment path = {enrollment.Path}");
+            }
 
             await host.RunAsync();
         }
 
         static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args);
+            Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((hostingContext, configuration) =>
+                {
+                    configuration.Sources.Clear();
+
+                    IHostEnvironment env = hostingContext.HostingEnvironment;
+
+                    configuration
+                        .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                        .AddJsonFile($"appsettings.{env.EnvironmentName}.json", true, true);
+
+                    IConfigurationRoot configurationRoot = configuration.Build();
+
+                    options = new DownloaderOptions();
+
+                    configurationRoot.GetSection(nameof(DownloaderOptions))
+                                     .Bind(options);
+                    
+                    var enrollments = configurationRoot.GetSection("Enrollments").GetChildren(); 
+                    foreach (var keyValuePair in enrollments) 
+                    {
+                        Enrollment newEnrollment = new Enrollment()
+                        {
+                            EnrollmentNumber = keyValuePair.Key,
+                            Path = keyValuePair.Value
+                        };
+                        enrollmentList.Add(newEnrollment);
+                    }
+                });
+    }
+
+    public class DownloaderOptions {
+        public string BearerToken {get;set;}
+        public int NumberOfMonthsToDownload {get;set;} 
+    }
+
+    public class Enrollment {
+        public string EnrollmentNumber {get;set;}
+        public string Path {get;set;}
     }
 }
